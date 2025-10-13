a2f545bfc7ebf8dcbda7bf9117e7b77a7c7d386d
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-10-13 15:25:16 +0700

    chore: remove cloudflare deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
 };
 
 export default nextConfig;
-
-// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
-initOpenNextCloudflareForDev();
